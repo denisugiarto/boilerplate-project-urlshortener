788f3ab648afbc350ac53894169177c62ec29b38
Deni Sugiarto <sugiarto.deni@gmail.com>
2025-08-15 14:52:10 +0700

    done

--- index.js
+++ index.js
@@ -1,24 +1,64 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const dns = require("dns");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+const db = ["test"];
 
 app.use(cors());
+app.use(bodyParser.urlencoded());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.post("/api/shorturl", (req, res) => {
+  const url = req.body.url;
+  // Check if URL is provided
+  if (!url) {
+    return res.json({ error: "invalid url" });
+  }
+
+  let parsedUrl;
+  try {
+    parsedUrl = new URL(url);
+  } catch (err) {
+    return res.json({ error: "invalid url" });
+  }
+
+  // Check if protocol is http or https
+  if (!["http:", "https:"].includes(parsedUrl.protocol)) {
+    return res.json({ error: "invalid url" });
+  }
+
+  dns.lookup(parsedUrl.hostname, (err) => {
+    if (err) return res.json({ error: "invalid url" });
+    db.push(url);
+    console.log(db);
+    const index = db.findIndex((item) => item === url);
+    res.json({
+      original_url: url,
+      short_url: index,
+    });
+  });
+});
+
+app.get("/api/shorturl/:short_url?", (req, res) => {
+  if (!req.params.short_url) return res.json({ error: "invalid url" });
+  res.redirect(db?.[req.params.short_url]);
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
